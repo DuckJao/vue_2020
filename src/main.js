a3709842909f058f279721e400410308503efea9
JackDou <jackdou@gmail.com>
2020-08-15 20:04:42 +0800

    修改設定

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue';
+
+// 套件要在app.vue之前添加，不然ESLint會報錯
+import ElementUI from 'element-ui';
+import axios from 'axios';
+import api from '@/service/api'; // 第二步驟會用到
+import VueAxios from 'vue-axios';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css'; // 一定要在最後面，不然會被ESLin報錯
 
+Vue.config.productionTip = true;
+
+// use是使用套件，套用並設定prototype
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
+Vue.prototype.$api = api; // 定義api這個常數給AXIOS存取json-server或實際api環境用
 
 new Vue({
   router,
   store,
+  el: '#app',
   render: (h) => h(App),
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,5 +14,17 @@ export default {
   components: {
     HelloWorld,
   },
+  methods: {
+    async packageGetData() {
+      const url = 'tableData'; // json-server  API 位置
+      const res = await this.$api.get(url);
+      this.tableData = [...res]; // 透過ES6語法將res的內容直接繼承到tableData
+      console.log(res);
+    },
+  },
+
+  created() {
+    this.packageGetData(); // 在created 階段把API資料叫進來
+  },
 };
 </script>
